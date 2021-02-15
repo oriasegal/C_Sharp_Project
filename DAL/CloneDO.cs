856738a6788f9350c9ca1b772435aaf6e917e49d
oriasegal <73060217+oriasegal@users.noreply.github.com>
2021-02-15 17:56:25 +0200

    DAL tier

---
+++ DAL/CloneDO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using DO;
+
+namespace DO
+{
+    public static class CloneDO
+    {
+        internal static T Clone<T>(this T original)
+        {
+            T result = (T)Activator.CreateInstance(typeof(T));
+            foreach (PropertyInfo item in original.GetType().GetProperties())
+            {
+                item.SetValue(result, item.GetValue(original, null));
+            }
+            return result;
+        }
+    }
+}
